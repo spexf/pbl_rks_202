5c54f764321002e99ee490b77e6618e87f0fd430
ath.thoriq <49811982+spexf@users.noreply.github.com>
2024-07-02 16:02:55 +0700

    Delete bot/tes2.py

--- bot/tes2.py
+++
@@ -1,6 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(4)
-print(s.connect(('192.168.109.145', 80)))
-s.send_header("Accept-language", "en-US,en,q=0.5")
-print(s)
